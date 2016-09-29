31ce1cf534b5c47815c163518967033079232dfd
Hector Ivan Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2016-09-29 01:42:59 -0500

    session creation

--- boilerplate/web/controllers/session_controller.ex
+++ boilerplate/web/controllers/session_controller.ex
@@ -5,5 +5,16 @@ defmodule Boilerplate.SessionController do
     render("new.html")
   end
 
-
+  def create(conn, %{"session" => %{"username" => username, "password" => pass}}) do
+    case Boilerplate.Auth.login_by_username_and_password(conn, username, pass, repo:Repo) do
+      {:ok, conn} ->
+        conn
+        |> put_flash(:info, "Welcome back!")
+        |>redirect(to: page_path(conn, :index))
+      {:error, _reason, conn}->
+        conn
+        |> put_flash(:error, "Bad user or password!")
+        |>render("new.html")
+    end
+  end
 end
